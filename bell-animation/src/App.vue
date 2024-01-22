95fdfc7bf132a3f95d9592ed9138662563f66e6c
Taweria <elodieali.pro@gmail.com>
2024-01-22 10:41:21 +0100

    animation bell

---
+++ bell-animation/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Bell from './components/Bell.vue'
+</script>
+
+<template>
+
+  <main>
+    <Bell />
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ bell-animation/src/components/Bell.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+<template>
+  <img src="../assets/notifications.svg" alt="bell">
+</template>
+
+<style scoped>
+img{
+    width: 150px;
+    height: 150px;
+    animation: bell 1.5s ease-in-out infinite alternate;
+
+}
+
+@keyframes bell {
+    0% {
+
+        transform: rotate(0deg);
+
+    }
+
+    5%, 15% {
+
+        transform: rotate(25deg);
+
+    }
+
+    10%, 20% {
+
+        transform: rotate(-25deg);
+
+    }
+
+    25%  {
+
+        transform: rotate(0deg);
+
+    }
+
+    100% {
+
+        transform: rotate(0deg);
+
+    }
+}
+</style>
